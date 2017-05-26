1db06f49e4df58d7c4505c6768fe7da0ed55a4f5
vaquierm <michael.vaquier@mail.mcgill.ca>
2017-05-26 09:12:42 -0400

    battery channel functional

--- app.js
+++ app.js
@@ -264,7 +264,6 @@ function turnLightSensorOn(peripheral){
 function turnBatteryReadOn(peripheral){
 
       batteryDataChar.on('data', function(data, isNotification) {
-        console.log(data);
             var batteryLevel = data.readUInt8(0);
             console.log('[BLE] ' + peripheral.advertisement['localName'] + ' -> Battery Level : ' + batteryLevel + ' %');
             deviceClient.publishDeviceEvent("Enviro", peripheral.address.replace(/:/g, ''),"battery","json",'{"d" : { "light" : ' + batteryLevel + ' }}');

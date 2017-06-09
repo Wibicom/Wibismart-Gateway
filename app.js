6396d134d6129fb6fb6e5523414f37d131b788ce
vaquierm <michael.vaquier@mail.mcgill.ca>
2017-06-09 15:35:50 -0400

    testing

--- app.js
+++ app.js
@@ -154,7 +154,6 @@ deviceClient.on('connect', function () {
         break;
       case 'sensorPeriod':
         var targetDevice = connectedDevices[payload.data.deviceId];
-        console.log("target device: " + targetDevice + " ,connectedDevice: " + JSON.stringify(connectedDevices)+ " ,discovered devices: " + currentDiscoveredDevices);
         if(targetDevice == undefined || targetDevice == null) {
            deviceClient.publishGatewayEvent("sensorToggleResponse", 'json', JSON.stringify({message: "The device " + payload.data.localName + " is not connected, you cannot change the period of its sensors."}));
            break;

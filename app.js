77959ba8c979c097b026768ead8340d264e4f6bb
vaquierm <michael.vaquier@mail.mcgill.ca>
2017-06-26 13:44:07 -0400

    a

--- app.js
+++ app.js
@@ -450,7 +450,7 @@ function connectToEnviro(peripheral) {
 
 function setPeriod(char, period, peripheral, charName){
 	  var periodBuf = new Buffer(2);
-    periodBuf.writeUInt16(period, 0);
+    periodBuf.writeUInt16LE(period, 0);
     if(char) {
       char.write(periodBuf, false, function(err) {
         if(err) {
